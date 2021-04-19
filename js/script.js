6794e7fd9daf94b8c3e8f99c7ed4157512f2e022
Nazmul Hasan Robin <robin.sust08@gmail.com>
2021-04-19 11:28:32 +0600

    special menu items working now

--- js/script.js
+++ js/script.js
@@ -102,6 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
